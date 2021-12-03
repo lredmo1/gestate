bdd19293d7484696897d8e73e781f7571d10d2ba
Zachary <zvcoding@gmail.com>
2021-12-03 18:28:43 -0500

    one level down refs added, should add width and height to ref for canvas

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,12 @@
 
 import './App.css';
-import { useEffect, useRef, useState } from 'react'
+import CanvasMain from './CanvasMain';
 
 
 
 function App() {
-
-  const canvasRef = useRef(null)
-  const contextRef = useRef(null)
-  const [isDrawing, setIsDrawing] = useState(false)
-
-  useEffect(()=>{
-    const canvas = canvasRef.current
-    canvas.width = window.innerWidth * 2;
-    canvas.height = window.innerHeight * 2;
-    canvas.style.width = `${window.innerWidth}px`
-    canvas.style.height = `${window.innerHeight}px`
-
-    const ctx = canvas.getContext("2d")
-    ctx.scale(2,2)
-    ctx.lineCap = "round"
-    ctx.strokeStyle = "black"
-    ctx.lineWidth = 5
-    contextRef.current = ctx;
-
-  },[])
-
-  const startPath = ({nativeEvent}) => {
-    const {offsetX, offsetY} = nativeEvent;
-    contextRef.current.beginPath()
-    contextRef.current.moveTo(offsetX, offsetY)
-    setIsDrawing(true)
-  }
-  
-  const finishPath = () => {
-    contextRef.current.closePath()
-    setIsDrawing(false)
-  }
-  
-  const drawPath = ({nativeEvent}) => {
-    if(!isDrawing){
-      return
-    }
-  const {offsetX, offsetY} = nativeEvent; 
-  contextRef.current.lineTo(offsetX, offsetY)
-  contextRef.current.stroke()
-  }
-
   return (
-   <canvas
-    onMouseDown = {startPath}
-    onMouseUp = {finishPath}
-    onMouseMove = {drawPath}
-    ref = {canvasRef}
-    className = "canvasMain"
-
-    />
+  <CanvasMain />
   );
 }
 
