a21adb0d62b43dfde886efda8038449de89f6659
Lisa <redlisa125@gmail.com>
2021-12-06 08:13:51 -1000

    color change functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,43 @@
-
-import './App.css';
-import CanvasMain from './CanvasMain';
-
-
+import "./App.css";
+import { useState } from "react"
+import CanvasMain from "./CanvasMain";
 
 function App() {
+
+  const [contextColor, setContextColor] = useState("pink") 
+
+const handleColorChangeGreen = (e) => {
+  setContextColor("green")
+  console.log("you werk?")
+  console.log(contextColor)
+}
+
+const handleColorChangeYellow = (e) => {
+  setContextColor("yellow")
+  console.log("you werk?")
+  console.log(contextColor)
+}
+
+const handleColorChangeRed = (e) => {
+  setContextColor("red")
+  console.log("you werk?")
+  console.log(contextColor)
+}
+
+const handleColorChangeErase = (e) => {
+  setContextColor("white")
+  console.log("you werk?")
+  console.log(contextColor)
+}
+
   return (
-  <CanvasMain />
+    <>
+      <CanvasMain CurrentColor={contextColor}/>
+      <button onClick={handleColorChangeGreen}>Green</button>
+      <button onClick={handleColorChangeYellow}>Yellow</button>
+      <button onClick={handleColorChangeRed}>Red</button>
+      <button onClick={handleColorChangeErase}>Erase</button>
+    </>
   );
 }
 
