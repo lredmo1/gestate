f4497c15d0960d1b1a710e3289f37aecb25b74d6
Lisa <redlisa125@gmail.com>
2021-12-06 08:32:28 -1000

    erase feature

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import CanvasMain from "./CanvasMain";
 function App() {
 
   const [contextColor, setContextColor] = useState("pink") 
+  const [lineWidth, setLineWidth] = useState(5) 
 
 const handleColorChangeGreen = (e) => {
   setContextColor("green")
@@ -30,13 +31,38 @@ const handleColorChangeErase = (e) => {
   console.log(contextColor)
 }
 
+const handleSmallLine = (e) => {
+  setLineWidth(1)
+}
+
+const handleMediumLine = (e) => {
+  setLineWidth(5)
+}
+
+const handleLargeLine = (e) => {
+  setLineWidth(10)
+}
+
+const handleExtraLargeLine = (e) => {
+  setLineWidth(20)
+}
+
+const handleSuperLine = (e) => {
+  setLineWidth(100)
+}
+
   return (
     <>
-      <CanvasMain CurrentColor={contextColor}/>
+      <CanvasMain currentColor={contextColor} currentWidth={lineWidth}/>
       <button onClick={handleColorChangeGreen}>Green</button>
       <button onClick={handleColorChangeYellow}>Yellow</button>
       <button onClick={handleColorChangeRed}>Red</button>
       <button onClick={handleColorChangeErase}>Erase</button>
+      <button onClick={handleSmallLine}>Small</button>
+      <button onClick={handleMediumLine}>Medium</button>
+      <button onClick={handleLargeLine}>Large</button>
+      <button onClick={handleExtraLargeLine}>Extra Large</button>
+      <button onClick={handleSuperLine}>Super</button>
     </>
   );
 }
