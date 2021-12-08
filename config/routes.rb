f80e07e0c351e3c5ae8201bcbe9176351b4fc9c2
Lisa <redlisa125@gmail.com>
2021-12-07 16:32:56 -1000

    added assssociations in models

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
+  post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   get "/me", to: "users#show"
   delete "/logout", to: "sessions#destroy"
